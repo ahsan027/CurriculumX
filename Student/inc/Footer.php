c04731e2e83730ae07408ba53be307d5e25a7f3b
whitewolf <ahsanauddry.ndc@gmail.com>
2024-12-23 13:04:35 +0600

    Changed Footer

--- Student/inc/Footer.php
+++ Student/inc/Footer.php
@@ -1,5 +1,10 @@
 <footer class="main-footer">
-      <h4>RCD2013C - EduPulse&copy;2024</h4>
+      <h4>CurriculumX&copy;2024</h4>
+      <h4>ahsan</h4>
+      <h4>ahsan</h4>
+      <h4>ahsan</h4>
+
+
     </footer>
     <script src="../assets/js/bootstrap.bundle.min.js" ></script>
   </body>
